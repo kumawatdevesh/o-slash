6a3c6b938f8d2c8dac3e532603abba9ac84fe4ad
Devesh Kumawat <devesh@lal10.com>
2022-05-31 15:24:41 +0530

    test cases for use authentication controller

---
+++ __tests__/user.spec.ts
@@ -0,0 +1,129 @@
+import {knex} from '../database/knex'
+import {agent as request} from 'supertest'
+import {app} from '../app'
+import { ILoginReqObject, IRegisterReqObject } from '../interfaces/auth';
+import { errorResponse } from '../middlewares/error';
+import { ErrorResponse } from '../utils/error';
+jest.useFakeTimers('legacy')
+
+describe('The register and login process', () => {
+
+    const REGISTER_ENDPOINT: string='/api/v1/register';
+    const LOGIN_ENDPOINT: string='/api/v1/login';
+    let userRegObj: IRegisterReqObject;
+    let userLoginObj: ILoginReqObject;
+    let req;
+    let res;
+
+    beforeAll(async () => {
+        await knex('users').del().where({email: "test@gmail.com"})
+
+        userRegObj = {
+            email: "test@gmail.com",
+            password: "test",
+            fname: "test",
+            lname: "last"
+        }
+
+        userLoginObj = {
+            email: "test@gmail.com",
+            password: "test"
+        }
+
+        req = {
+            params: {},
+            body: {}
+        }
+      
+        res = {
+            data: null,
+            code: null,
+            status (status) {
+                this.code = status
+                return this
+            },
+            json (payload) {
+                this.data = payload
+            }
+        }
+    })
+
+    it('it should throw an error if required field is not present', async () => {
+
+        const next = jest.fn()
+        const err: ErrorResponse = {
+            message: "please enter email with at least 3 char", 
+            statusCode: 400,
+            path: REGISTER_ENDPOINT,
+            success: false,
+            name: ""
+        }
+
+        errorResponse(err, req, res, next)
+
+        expect(res.data.error).toBe('please enter email with at least 3 char')
+        expect(res.data.statusCode).toBe(400)
+        expect(res.data.success).toBe(false)
+        expect(res.data.path).toBe(REGISTER_ENDPOINT)
+    })
+
+    it('it should register a new user', async () => {
+
+        await request(app).post(REGISTER_ENDPOINT).send(userRegObj)
+        .then(response => {
+            expect(response.status).toBe(201);
+            expect(response.body.message).toBe('User successfully registered!')
+            expect(response.body.code).toBe(201)
+            expect(response.body.success).toBe(true)
+        })
+    })
+
+    it('should return a 422 if user already exists', async () => {
+
+        await request(app).post(REGISTER_ENDPOINT).send(userRegObj)
+        .then(response => {
+            expect(response.status).toBe(422);
+            expect(response.body.message).toBe('Account with email exists!')
+            expect(response.body.code).toBe(422)
+            expect(response.body.success).toBe(true)
+        })
+    })
+
+    it('it should login a user', async () => {
+
+        await request(app).post(LOGIN_ENDPOINT).send(userLoginObj)
+        .then(response => {
+            expect(response.status).toBe(200)
+            expect(response.body.data.token).toBeDefined()
+            expect(response.body.message).toBe('Success')
+            expect(response.body.code).toBe(200)
+            expect(response.body.success).toBe(true)
+        })
+    })
+
+    it('should return 404 if user do not exists', async () => {
+
+        await request(app).post(LOGIN_ENDPOINT).send({...userLoginObj, email: "iam@gmail.com"})
+        .then(response => {
+            expect(response.status).toBe(404)
+            expect(response.body.message).toBe('User with email do not exist!')
+            expect(response.body.code).toBe(404)
+            expect(response.body.success).toBe(true)
+        })
+    })
+
+    it('should return 401 if password is incorrect', async () => {
+
+        await request(app).post(LOGIN_ENDPOINT).send({...userLoginObj, password: "dkjgdjgdj"})
+        .then(response => {
+            expect(response.status).toBe(401)
+            expect(response.body.message).toBe('Password is incorrect!')
+            expect(response.body.code).toBe(401)
+            expect(response.body.success).toBe(true)
+        })
+    })
+
+    afterAll(async () => {
+        await knex.destroy()
+    })
+})
--- utils/error.ts
+++ utils/error.ts
@@ -4,11 +4,13 @@ class ErrorResponse extends Error {
 
     public statusCode: number
     public path: string
+    public success: boolean
 
     constructor(response: IErrorResponse) {
         super(response.error);
         this.statusCode = response.statusCode
         this.path = response.path
+        this.success = response.success
     }
 }
 
